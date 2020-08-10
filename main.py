90cbdabe1ed112ae752a53babb97b4a4ffbdc830
daliborstakic <stakicdalibor64@gmail.com>
2020-08-10 19:06:11 +0200

    Added ping output in the console.

--- main.py
+++ main.py
@@ -1,10 +1,17 @@
 import time
 from pythonping import ping
 
+hostname = '192.168.1.1'
+
 
 def main():
     while True:
-        pass
+        response_list = ping(hostname, verbose=True)
+
+        for response in response_list:
+            print(response.time_elapsed_ms)
+
+        time.sleep(1)
 
 
 if __name__ == '__main__':
