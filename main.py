7e8cbd32278d9f2006729e40d039dcc231686fb4
daliborstakic <stakicdalibor64@gmail.com>
2020-08-11 10:55:23 +0200

    The graph is animated.

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from pythonping import ping
 from itertools import count
-from matplotlib import pyplot
+from matplotlib import pyplot as plt
 from matplotlib.animation import FuncAnimation
 
 hostname = '192.168.1.1'
@@ -13,3 +13,22 @@ index = count()
 
 def ping_host():
     response_list = ping(hostname)
+    return float(response_list.rtt_avg_ms)
+
+
+def animate(i):
+    x_values.append(next(index))
+    y_values.append(ping_host())
+
+    plt.cla()
+    plt.plot(x_values, y_values)
+
+
+ani = FuncAnimation(plt.gcf(), animate, interval=1000)
+
+plt.tight_layout()
+
+plt.xlabel('Time (s)')
+plt.ylabel('Ping (ms)')
+
+plt.show()
